20b6cf67d170ee62130ade2cbab55ad284d99c6b
Gilang <gilangmaulana541@gmail.com>
2024-04-01 15:27:31 +0700

    responsive user guide

--- resources/views/pages/userGuide.blade.php
+++ resources/views/pages/userGuide.blade.php
@@ -15,148 +15,169 @@ User Settings
     <div class="col-md-12">
         <div class="card mb-4">
             <h4 class="card-header">Petunjuk Pemakaian</h5>
-            <div class="card-body">
+                <div class="card-body">
+
                     <div class="row">
                         <hr>
-                        <h5>Autentikasi</h4>
-                        <hr>  
-                        <div class="col-3 mb-4">
-                            <h6>Register:</h6>
-                            <ul class="ps-3 mb-0">
-                                <li class="mb-1">
-                                    Klik Daftar akun
-                                </li>
-                                <li class="mb-1">
-                                   Isi form registrasi
-                                </li>
-                                <li class="mb-1">
-                                   klik tombol Daftar
-                                </li>
-                            </ul>
-                        </div>
-                        <div class="col-3 mb-4">
-                            <h6>Login:</h6>
-                            <ul class="ps-3 mb-0">
-                                <li class="mb-1">
-                                    Isi form email dan password
-                                </li>
-                                <li class="mb-1">
-                                 Klik tombol Masuk
-                                </li>
-                            </ul>
-                        </div>
-                        <div class="col-3 mb-4">
-                            <h6>Forgot:</h6>
-                            <ul class="ps-3 mb-0">
-                                <li class="mb-1">
-                                    Klik Lupa Password
-                                </li>
-                                <li class="mb-1">
-                                 Isi form email
-                                 </li>
-                                 <li class="mb-1">
-                                 Klik Kirim Link Reset
-                                 </li>
-                            </ul>
-                        </div>
-                        <div class="col-3 mb-4">
-                            <h6>Reset:</h6>
-                            <ul class="ps-3 mb-0">
-                                <li class="mb-1">
-                                    Cek gmail, buka pesan email dari TU Fasilkom Unsika
-                                </li>
-                                <li class="mb-1">
-                                    Klik Reset Password
-                                </li>
-                                <li class="mb-1">
-                                    Isi form password baru dan konfirmasi password baru
-                                </li>
-                                <li class="mb-1">
-                                    Klik Set Password Baru
-                                </li>
-                            </ul>
-                        </div>
+                        <h5>Autentikasi
+            </h4>
+            <hr>
+            <div class="col-md-12">
+                <div class="row">
+                    <div class="col-sm-6 mb-4">
+                        <!-- col-sm-6 untuk layar kecil, 2 item per baris -->
+                        <h6>Register:</h6>
+                        <ul class="ps-3 mb-0">
+                            <li class="mb-1">
+                                Klik Daftar akun
+                            </li>
+                            <li class="mb-1">
+                                Isi form registrasi
+                            </li>
+                            <li class="mb-1">
+                                klik tombol Daftar
+                            </li>
+                        </ul>
                     </div>
-                     <div class="row">
-                         <hr>
-                           <h5>Menu Mahasiswa</h4>
-                        <hr> 
-                        <div class="col-2 mb-4">
+                    <div class="col-sm-6 mb-4">
+                        <!-- col-sm-6 untuk layar kecil, 2 item per baris -->
+                        <h6>Login:</h6>
+                        <ul class="ps-3 mb-0">
+                            <li class="mb-1">
+                                Isi form email dan password
+                            </li>
+                            <li class="mb-1">
+                                Klik tombol Masuk
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="col-sm-6 mb-4">
+                        <!-- col-sm-6 untuk layar kecil, 2 item per baris -->
+                        <h6>Forgot:</h6>
+                        <ul class="ps-3 mb-0">
+                            <li class="mb-1">
+                                Klik Lupa Password
+                            </li>
+                            <li class="mb-1">
+                                Isi form email
+                            </li>
+                            <li class="mb-1">
+                                Klik Kirim Link Reset
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="col-sm-6 mb-4">
+                        <!-- col-sm-6 untuk layar kecil, 2 item per baris -->
+                        <h6>Reset:</h6>
+                        <ul class="ps-3 mb-0">
+                            <li class="mb-1">
+                                Cek gmail, buka pesan email dari TU Fasilkom Unsika
+                            </li>
+                            <li class="mb-1">
+                                Klik Reset Password
+                            </li>
+                            <li class="mb-1">
+                                Isi form password baru dan konfirmasi password baru
+                            </li>
+                            <li class="mb-1">
+                                Klik Set Password Baru
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+        <div class="row">
+            <hr>
+            <h5>Menu Mahasiswa</h4>
+                <hr>
+                <div class="col-md-6">
+                    <div class="row">
+                        <div class="col-12 mb-4">
                             <h6>Pembuatan Surat:</h6>
                             <ul class="ps-3 mb-0">
                                 <li class="mb-1">
                                     Pilih surat yang ingin dibuat
                                 </li>
                                 <li class="mb-1">
-                                   Isi dan fengkapi form
+                                    Isi dan fengkapi form
                                 </li>
                                 <li class="mb-1">
-                                   Klik Send
+                                    Klik Send
                                 </li>
                             </ul>
                         </div>
-                        <div class="col-3 mb-4">
+                        <div class="col-12 mb-4">
                             <h6>Edit Profile:</h6>
                             <ul class="ps-3 mb-0">
                                 <li class="mb-1">
-                                     Klik gambar profil user pojok kanan atas muncul dropdown lalu klik Pengaturan
+                                    Klik gambar profil user pojok kanan atas muncul dropdown lalu klik Pengaturan
                                 </li>
                                 <li class="mb-1">
-                                 Klik tombol data diri
+                                    Klik tombol data diri
                                 </li>
                                 <li class="mb-1">
-                                 Isi dan lengkapi form
+                                    Isi dan lengkapi form
                                 </li>
                                 <li class="mb-1">
-                                Klik Update Profile
+                                    Klik Update Profile
                                 </li>
                             </ul>
                         </div>
-                        <div class="col-3 mb-4">
-                            <h6>Setting Akun:</h6>
-                            <ul class="ps-3 mb-0">
-                                <li class="mb-1">
-                                      Klik gambar profil user pojok kanan atas muncul dropdown  lalu klik Pengaturan
-                                </li>
-                                <li class="mb-1">
-                                 Klik tombol Akun                                 </li>
-                                 <li class="mb-1">
-                                 Isi dan lengkapi form
-                                 </li>
-                                 <li class="mb-1">
-                                 Klik Save Changes
-                                 </li>
-                            </ul>
-                        </div>
-                        <div class="col-2 mb-4">
+                        <div class="col-12 mb-4">
                             <h6>Lihat Profile:</h6>
                             <ul class="ps-3 mb-0">
                                 <li class="mb-1">
-                                     Klik gambar profil user pojok kanan atas muncul dropdown 
+                                    Klik gambar profil user pojok kanan atas muncul dropdown
                                 </li>
                                 <li class="mb-1">
                                     Klik Profile
                                 </li>
                             </ul>
                         </div>
-                         <div class="col-2 mb-4">
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="row">
+                        <div class="col-12 mb-4">
+                            <h6>Setting Akun:</h6>
+                            <ul class="ps-3 mb-0">
+                                <li class="mb-1">
+                                    Klik gambar profil user pojok kanan atas muncul dropdown lalu klik Pengaturan
+                                </li>
+                                <li class="mb-1">
+                                    Klik tombol Akun
+                                </li>
+                                <li class="mb-1">
+                                    Isi dan lengkapi form
+                                </li>
+                                <li class="mb-1">
+                                    Klik Save Changes
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="col-12 mb-4">
                             <h6>Download Surat:</h6>
                             <ul class="ps-3 mb-0">
                                 <li class="mb-1">
                                     Klik Riwayat Surat
                                 </li>
                                 <li class="mb-1">
-                                   Pilih jenis surat yang telah dibuat
+                                    Pilih jenis surat yang telah dibuat
                                 </li>
                                 <li class="mb-1">
-                                   Klik tombol Download di tabel kolom aktivitas
+                                    Klik tombol Download di tabel kolom aktivitas
                                 </li>
                             </ul>
                         </div>
                     </div>
-            </div>
+                </div>
         </div>
-        <!--/ Change Password -->
     </div>
+</div>
+<!--/ Change Password -->
+</div>
 </div>
 @endsection
